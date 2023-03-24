7407a826479df83e530fccebed8dc6ba653650fd
sourov <sourovch@gmail.com>
2023-03-25 02:01:10 +0600

    added form validation

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useLogin from '../hooks/useLogin';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 const coverImg = `/images/login_page_img.webp`;
 
 const Login = () => {
   const { loading, login } = useLogin();
-  const [formValue, setFormValue] = useState({
-    email: '',
-    password: '',
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit(values) {
+      login(values.email, values.password);
+    },
+    validationSchema: yup.object({
+      email: yup
+        .string()
+        .email('Invalid email')
+        .required('Email is required'),
+      password: yup
+        .string()
+        .min(8, 'Invalid password')
+        .required('Password is required'),
+    }),
   });
 
   useEffect(() => {
@@ -17,18 +34,6 @@ const Login = () => {
     return () => (document.title = '');
   }, []);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    login(formValue.email, formValue.password);
-  };
-
-  const handleInputChange = (e) => {
-    const element = e.target;
-
-    setFormValue((v) => ({ ...v, [element.name]: element.value }));
-  };
-
   return (
     <>
       <div>
@@ -36,27 +41,49 @@ const Login = () => {
           <h1>ToDos</h1>
           <h2>An awesome way to manage your daily doings</h2>
         </hgroup>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email">
+        <form onSubmit={formik.handleSubmit}>
+          <label
+            htmlFor="email"
+            aria-invalid={
+              formik.touched.email && !!formik.errors.email
+            }
+          >
+            {formik.touched.email && formik.errors.email
+              ? formik.errors.email
+              : 'Email'}
             <input
               type="text"
               id="email"
               name="email"
               placeholder="Enter email"
-              value={formValue.email}
-              onChange={handleInputChange}
-              required
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              aria-invalid={
+                formik.touched.email && !!formik.errors.email
+              }
             />
           </label>
-          <label htmlFor="password">
+          <label
+            htmlFor="password"
+            aria-invalid={
+              formik.touched.password && !!formik.errors.password
+            }
+          >
+            {formik.touched.password && formik.errors.password
+              ? formik.errors.password
+              : 'Password'}
             <input
               type="password"
               id="password"
               name="password"
               placeholder="Enter your password"
-              value={formValue.password}
-              onChange={handleInputChange}
-              required
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              aria-invalid={
+                formik.touched.password && !!formik.errors.password
+              }
             />
           </label>
           <button
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,35 +3,102 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import useLogin from '../hooks/useLogin';
 import { toast } from 'react-toastify';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import YupPassword from 'yup-password';
 
 import { ERROR_TOAST_OPTIONS } from '../utils/tostOptions';
 import { CREATE_USER_QUERY } from '../utils/gqlQuerys';
+YupPassword(yup);
 
 const coverImg = '/images/reg_page_img.webp';
 const uploadPlaceholder = '/images/upload_placeholder.webp';
 
 const Register = () => {
   const { loading: loginLoad, login } = useLogin();
-  const [selectedImg, setSelectedImg] = useState(undefined);
   const [uploadPreview, setUploadPriview] =
     useState(uploadPlaceholder);
-  const [formValue, setFormValue] = useState({
-    name: '',
-    email: '',
-    password: '',
-    conformation: '',
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      conformation: '',
+      selectedImg: undefined,
+    },
+    onSubmit({ name, email, password, selectedImg }) {
+      let qureyData = {
+        name,
+        email,
+        password,
+      };
+
+      if (selectedImg) {
+        qureyData = {
+          ...qureyData,
+          image: {
+            upload: selectedImg,
+          },
+        };
+      }
+
+      reqCreateUser({
+        variables: {
+          data: qureyData,
+        },
+        onCompleted: ({ createUser: user }) => {
+          login(user.email, password);
+        },
+
+        onError: (err) => {
+          toast.error(err.message, ERROR_TOAST_OPTIONS);
+        },
+      });
+    },
+    validationSchema: yup.object({
+      name: yup
+        .string()
+        .min(3, "Name can't be that small")
+        .max(20, 'To large for a name')
+        .required('Please enter your name'),
+      email: yup
+        .string()
+        .email('Invalid email')
+        .required('Please enter your email'),
+      password: yup
+        .string()
+        .min(8, 'Password must contain 8+ characters')
+        .minLowercase(
+          1,
+          'Password must contain at least 1 lower case letter'
+        )
+        .minUppercase(
+          1,
+          'Password must contain at least 1 upper case letter'
+        )
+        .minNumbers(1, 'Password must contain at least 1 number')
+        .minSymbols(
+          1,
+          'Password must contain at least 1 special character'
+        )
+        .required('Password Is required'),
+      conformation: yup
+        .string()
+        .oneOf([yup.ref('password'), null], 'Passwords must match')
+        .required('Please confirm'),
+    }),
   });
   const [reqCreateUser, { loading: regLoad }] =
     useMutation(CREATE_USER_QUERY);
 
   useEffect(() => {
-    if (!selectedImg) return;
+    if (!formik.values.selectedImg) return;
 
-    const objectUrl = URL.createObjectURL(selectedImg);
+    const objectUrl = URL.createObjectURL(formik.values.selectedImg);
     setUploadPriview(objectUrl);
 
     return () => URL.revokeObjectURL(objectUrl);
-  }, [selectedImg]);
+  }, [formik.values.selectedImg]);
 
   useEffect(() => {
     document.title = 'Register';
@@ -41,51 +108,9 @@ const Register = () => {
 
   const onSelectFile = (e) => {
     if (!e.target.files || e.target.files.length === 0)
-      return setSelectedImg(undefined);
+      return formik.setFieldValue('selectedImg', undefined);
 
-    setSelectedImg(e.target.files[0]);
-  };
-
-  const fromSubmitHandler = (e) => {
-    e.preventDefault();
-
-    const name = e.target['name'].value;
-    const email = e.target['email'].value;
-    const password = e.target['password'].value;
-
-    let qureyData = {
-      name,
-      email,
-      password,
-    };
-
-    if (selectedImg) {
-      qureyData = {
-        ...qureyData,
-        image: {
-          upload: selectedImg,
-        },
-      };
-    }
-
-    reqCreateUser({
-      variables: {
-        data: qureyData,
-      },
-      onCompleted: ({ createUser: user }) => {
-        login(user.email, password);
-      },
-
-      onError: (err) => {
-        toast.error(err.message, ERROR_TOAST_OPTIONS);
-      },
-    });
-  };
-
-  const handleInputChange = (e) => {
-    const element = e.target;
-
-    setFormValue((v) => ({ ...v, [element.name]: element.value }));
+    formik.setFieldValue('selectedImg', e.target.files[0]);
   };
 
   return (
@@ -95,7 +120,7 @@ const Register = () => {
           <h1>Welcome to ToDos</h1>
           <h2>Start tracking your daily doings</h2>
         </hgroup>
-        <form autoComplete="off" onSubmit={fromSubmitHandler}>
+        <form autoComplete="off" onSubmit={formik.handleSubmit}>
           <label htmlFor="display_image">
             Set display image
             <div className="upload_preview_wrapper">
@@ -109,53 +134,97 @@ const Register = () => {
               />
             </div>
           </label>
-          <label htmlFor="name">
-            Name
+          <label
+            htmlFor="name"
+            aria-invalid={formik.touched.name && !!formik.errors.name}
+          >
+            {formik.touched.name && formik.errors.name
+              ? formik.errors.name
+              : 'Name'}
             <input
               type="text"
               id="name"
               name="name"
               placeholder="Enter your name"
-              value={formValue.name}
-              onChange={handleInputChange}
-              required
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              aria-invalid={
+                formik.touched.name && !!formik.errors.name
+              }
             />
           </label>
-          <label htmlFor="email">
-            Email
+          <label
+            htmlFor="email"
+            aria-invalid={
+              formik.touched.email && !!formik.errors.email
+            }
+          >
+            {formik.touched.email && formik.errors.email
+              ? formik.errors.email
+              : 'Email'}
             <input
               type="email"
               id="email"
               name="email"
               placeholder="Enter your Email"
-              value={formValue.email}
-              onChange={handleInputChange}
-              required
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              aria-invalid={
+                formik.touched.email && !!formik.errors.email
+              }
             />
           </label>
           <div className="grid">
-            <label htmlFor="password">
+            <label
+              htmlFor="password"
+              aria-invalid={
+                formik.touched.password && !!formik.errors.password
+              }
+            >
               Password
               <input
                 type="password"
                 id="password"
                 name="password"
                 placeholder="New password"
-                value={formValue.password}
-                onChange={handleInputChange}
-                required
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                aria-invalid={
+                  formik.touched.password && !!formik.errors.password
+                }
               />
+              <small>
+                {formik.touched.password && formik.errors.password
+                  ? formik.errors.password
+                  : '*write a strong password for account safty'}
+              </small>
             </label>
-            <label htmlFor="conformation">
-              Confirm password
+            <label
+              htmlFor="conformation"
+              aria-invalid={
+                formik.touched.conformation &&
+                !!formik.errors.conformation
+              }
+            >
+              {formik.touched.conformation &&
+              formik.errors.conformation
+                ? formik.errors.conformation
+                : 'Confirm password'}
               <input
                 type="password"
                 id="conformation"
                 name="conformation"
                 placeholder="Confirm password"
-                value={formValue.conformation}
-                onChange={handleInputChange}
-                required
+                value={formik.values.conformation}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                aria-invalid={
+                  formik.touched.conformation &&
+                  !!formik.errors.conformation
+                }
               />
             </label>
           </div>
